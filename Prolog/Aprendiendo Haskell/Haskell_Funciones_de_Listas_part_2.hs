d12119717b38beece56f3ed6a20963501c26cef5
Tirso4 <Tirso4>
2016-12-09 21:47:42 -0800

    listas 2

---
+++ Prolog/Aprendiendo Haskell/Haskell_Funciones_de_Listas_part_2.hs
@@ -0,0 +1,22 @@
+
+rever = reverse[1,2,3,4,5]
+
+
+tomar x = take x [1,2,3,4,5]
+
+
+fuera x = drop x[1,2,3,4,5]
+
+mini = minimum  [1,2,3,4,5]
+
+
+maxi = maximum  [1,2,3,4,5]
+
+
+suma = sum [1..5]
+
+
+producto = product[1..5]
+
+
+elem x = x `elem` [1,2,3,4,5]
